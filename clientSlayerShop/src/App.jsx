35529996e70b61422bf3ff8aa72daa46c1f6691a
Gabriel Vieira <ing.gabrielvieira@gmail.com>
2024-08-29 20:53:13 +0200

    desarrollo de interfaz productos

--- clientSlayerShop/src/App.jsx
+++ clientSlayerShop/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Banner from "./components/Banner";
 import Category from "./components/Category";
 import NavBar from "./components/NavBar";
+import Producsts from "./components/Producsts";
 import SmallerBanners from "./components/SmallerBanners";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Banner />
       <SmallerBanners />
       <Category />
+      <Producsts />
     </>
   );
 }
---
+++ clientSlayerShop/src/components/Producsts.jsx
@@ -0,0 +1,178 @@
+function Producsts() {
+  return (
+    <div>
+      <div className="m-auto bg-whiteDemon h-1056 py-20 px-40">
+        <h1 className="text-black text-2xl pb-8">Nuevos Productos</h1>
+        <div className="flex justify-between gap-x-8 mb-5">
+          {/* NEZUKO */}
+          <div className="w-268 h-400 bg-violetDemon rounded-lg text-center">
+            <a href="#">
+              <img
+                className="w-48 m-auto mt-12"
+                src="https://imgbb.host/images/jd8VB.png"
+              />
+            </a>
+            <div className="">
+              <a href="#">
+                <h5 className="text-2xl font-medium text-white">
+                  Nezuko Kamado
+                </h5>
+                <p className="my-1.5 mb-4 font-semibold text-2xl text-white">
+                  $599
+                </p>
+              </a>
+              <div className="items-center">
+                <a
+                  href="#"
+                  className="mt-4 text-white active:bg-blackDemon border hover:bg-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Comprar ya
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* SHINAZUGAWUA */}
+          <div className="w-268 h-400 bg-violetDemon rounded-lg text-center">
+            <a href="#">
+              <img
+                className="w-48 h-253 m-auto mt-12"
+                src="https://imgbb.host/images/jyDzL.png"
+              />
+            </a>
+            <div className="">
+              <a href="#">
+                <h5 className="text-2xl font-medium text-white">
+                  Sanemi Shinazugawa
+                </h5>
+                <p className="my-1.5 mb-4 font-semibold text-2xl text-white">
+                  $599
+                </p>
+              </a>
+              <div className="items-center">
+                <a
+                  href="#"
+                  className="mt-4 text-white active:bg-blackDemon border hover:bg-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Comprar ya
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* RENGOKU */}
+          <div className="w-268 h-400 bg-violetDemon rounded-lg text-center">
+            <a href="#">
+              <img
+                className="w-48 h-253 m-auto mt-12"
+                src="https://imgbb.host/images/jywXk.png"
+              />
+            </a>
+            <div className="">
+              <a href="#">
+                <h5 className="text-2xl font-medium text-white">
+                  Kyojuro Rengoku
+                </h5>
+                <p className="my-1.5 mb-4 font-semibold text-2xl text-white">
+                  $599
+                </p>
+              </a>
+              <div className="items-center">
+                <a
+                  href="#"
+                  className="mt-4 text-white active:bg-blackDemon border hover:bg-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Comprar ya
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-between gap-x-8">
+          {/* gyomei */}
+          <div className="w-268 h-400 bg-violetDemon rounded-lg text-center">
+            <a href="#">
+              <img
+                className="m-auto mt-8"
+                src="https://imgbb.host/images/j2Uit.png"
+              />
+            </a>
+            <div className="">
+              <a href="#">
+                <h5 className="text-2xl font-medium text-white">
+                  Gyomei Himejima
+                </h5>
+                <p className="my-1.5 mb-4 font-semibold text-2xl text-white">
+                  $599
+                </p>
+              </a>
+              <div className="items-center">
+                <a
+                  href="#"
+                  className="mt-4 text-white active:bg-blackDemon border hover:bg-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Comprar ya
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* obanai */}
+          <div className="w-268 h-400 bg-violetDemon rounded-lg text-center">
+            <a href="#">
+              <img
+                className="w-48 m-auto mt-14"
+                src="https://imgbb.host/images/jxwaC.png"
+              />
+            </a>
+            <div className="">
+              <a href="#">
+                <h5 className="text-2xl font-medium text-white">
+                  Iguro Obanai
+                </h5>
+                <p className="my-1.5 mb-4 font-semibold text-2xl text-white">
+                  $599
+                </p>
+              </a>
+              <div className="items-center">
+                <a
+                  href="#"
+                  className="mt-4 text-white active:bg-blackDemon border hover:bg-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Comprar ya
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* MITSURI */}
+          <div className="w-268 h-400 bg-violetDemon rounded-lg text-center">
+            <a href="#">
+              <img
+                className="m-auto mt-8"
+                src="https://imgbb.host/images/jJ1MZ.png"
+              />
+            </a>
+            <div className="">
+              <a href="#">
+                <h5 className="text-2xl font-medium text-white">
+                  Mitsuri Kanroji
+                </h5>
+                <p className="my-1.5 mb-4 font-semibold text-2xl text-white">
+                  $599
+                </p>
+              </a>
+              <div className="items-center">
+                <a
+                  href="#"
+                  className="mt-4 text-white active:bg-blackDemon border hover:bg-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Comprar ya
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Producsts;
